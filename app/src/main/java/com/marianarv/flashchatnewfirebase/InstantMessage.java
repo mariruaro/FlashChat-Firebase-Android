fed78f5b1a6ddec305ca81506377181d2f302b95
marianavigano <mariana@fdd.com.br>
2018-06-30 19:43:15 -0300

    Initial commit

---
+++ app/src/main/java/com/marianarv/flashchatnewfirebase/InstantMessage.java
@@ -0,0 +1,27 @@
+package com.marianarv.flashchatnewfirebase;
+
+
+class InstantMessage {
+
+    private String message;
+    private String author;
+
+    InstantMessage(String message, String author) {
+        this.message = message;
+        this.author = author;
+    }
+
+    public InstantMessage() {
+
+
+
+    }
+
+    String getMessage() {
+        return message;
+    }
+
+    String getAuthor() {
+        return author;
+    }
+}
